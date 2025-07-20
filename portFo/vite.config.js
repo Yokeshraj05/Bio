e7b6236c6e11f45ba537564fe70d873bafd97548
Yokesh-CGN <yokesh@cognigennxt.com>
2025-07-20 15:50:14 +0530

    used hashrouter instead of browser router

--- portFo/vite.config.js
+++ portFo/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Bio/', // <-- replace this with your actual repo name
+  base: '/Bio/',
   plugins: [
     react(),
     tailwindcss(),
