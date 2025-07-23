e5b266240fd78f9626c12605fbe856578c08ceaf
Yokesh-CGN <yokesh@cognigennxt.com>
2025-07-23 23:10:27 +0530

    fixed bug

--- portFo/src/App.jsx
+++ portFo/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import { useLocation } from "react-router-dom";
